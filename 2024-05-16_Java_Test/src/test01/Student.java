e0f33ec84b6c93827c4da81466c65c53aa071fa7
crok130 <tnqls130001@gmail.com>
2024-05-17 09:41:27 +0900

    Java_Study_Test

--- 2024-05-16_Java_Test/src/test01/Student.java
+++ 2024-05-16_Java_Test/src/test01/Student.java
@@ -6,12 +6,13 @@ public class Student {
 	int grade;
 	String school;
 	
+	// 개발자가 정의한 생성자가 존재하면 컴파일러가 default 생성자가를 추가하지 않음
+	// 매개변수 없이 객체가 생성될 수 있도록 기본생성자 추가
 	public Student() {}
-	
+	// school, grade, name 필드 초기화에 필요한
+	// 3가지 인자값을 전달 받아 각 필드를 초기화하는 생성자 추가
 	
 	public Student(String school,int grade,String name) {
-		this.school = school;
-		this.grade= grade;
-		this.name = name;
+
 	}
 }
